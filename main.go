d4b21810de99a27b03cdf29df7684b2f3bc46794
Hector Correa <hector@hectorcorrea.com>
2017-07-30 16:01:50 -0400

    Adds support for -o (output) parameter to indicate fields to output

--- main.go
+++ main.go
@@ -6,16 +6,18 @@ import (
 	"strings"
 )
 
-var fileName, extract string
+var fileName, extract, output string
 
 func init() {
 	flag.StringVar(&fileName, "f", "", "MARC file to process. Required.")
-	flag.StringVar(&extract, "x", "", "Extract record where value is found.")
+	flag.StringVar(&extract, "x", "", "Extract record where string is found.")
+	flag.StringVar(&output, "o", "all", "Fields to output.")
 	flag.Parse()
 }
 
 func main() {
 	if fileName == "" {
+		fmt.Printf("marcli syntax:\r\n")
 		flag.PrintDefaults()
 		return
 	}
@@ -27,9 +29,14 @@ func main() {
 
 	var processor RecordProcessor
 	if extract != "" {
-		processor = ExtractProcessor{Value: strings.ToLower(extract)}
+		processor = ExtractProcessor{
+			Fields: output,
+			Value:  strings.ToLower(extract),
+		}
 	} else {
-		processor = ConsoleProcessor{}
+		processor = ConsoleProcessor{
+			Fields: output,
+		}
 	}
 
 	err = file.ReadAll(processor)
@@ -39,18 +46,23 @@ func main() {
 }
 
 type ConsoleProcessor struct {
+	Fields string
 }
 
 func (p ConsoleProcessor) Process(r Record) {
 	fmt.Printf("=LDR  %s (%d, %d, %d)\n", r.Leader, r.Pos, r.Leader.Length, r.Leader.DataOffset)
 	for _, v := range r.Values {
-		fmt.Printf("=%s  %s\r\n", v.Tag, v.Value)
+		// TODO: handle multiple comma delimited fields
+		if p.Fields == "all" || p.Fields == v.Tag {
+			fmt.Printf("=%s  %s\r\n", v.Tag, v.Value)
+		}
 	}
 	fmt.Printf("\r\n\r\n")
 }
 
 type ExtractProcessor struct {
-	Value string // value to search
+	Fields string
+	Value  string // value to search
 }
 
 func (p ExtractProcessor) Process(r Record) {
@@ -65,7 +77,10 @@ func (p ExtractProcessor) Process(r Record) {
 	if match {
 		fmt.Printf("=LDR  %s (%d, %d, %d)\n", r.Leader, r.Pos, r.Leader.Length, r.Leader.DataOffset)
 		for _, v := range r.Values {
-			fmt.Printf("=%s  %s\r\n", v.Tag, v.Value)
+			// TODO: handle multiple comma delimited fields
+			if p.Fields == "all" || p.Fields == v.Tag {
+				fmt.Printf("=%s  %s\r\n", v.Tag, v.Value)
+			}
 		}
 		fmt.Printf("\r\n\r\n")
 	}
