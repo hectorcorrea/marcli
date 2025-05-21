3963c2ed03c15a233c4bc2530474e1c5a5ead975
Hector Correa <hector_correa@princeton.edu>
2025-05-21 13:34:48 -0400

    Be explicit on the architecture for the executable

--- cmd/marcli/compile_all.sh
+++ cmd/marcli/compile_all.sh
@@ -1,3 +1,3 @@
-GOOS=darwin go build -o marcli
-GOOS=linux go build -o marcli_linux
+GOOS=darwin GOARCH=amd64 go build -o marcli
+GOOS=linux GOARCH=amd64 go build -o marcli_linux
 GOOS=windows GOARCH=386 go build -o marcli.exe
