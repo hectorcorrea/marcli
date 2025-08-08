a54ccfa09087e3a11819915477f706dee64a3aa9
Hector Correa <hector_correa@princeton.edu>
2025-08-08 15:28:55 -0400

    Set the version number when compiling the executables for a new release

--- cmd/marcli/compile_all.sh
+++ cmd/marcli/compile_all.sh
@@ -1,3 +1,14 @@
-GOOS=darwin GOARCH=amd64 go build -o marcli
-GOOS=linux GOARCH=amd64 go build -o marcli_linux
-GOOS=windows GOARCH=386 go build -o marcli.exe
+MARCLI_VERSION="$1"
+if [ "$MARCLI_VERSION" = "" ]; then
+  echo "Must pass a version number, e.g. v1.0.0"
+  exit 1
+fi
+
+echo "Compiling marcli $MARCLI_VERSION ..."
+# See this blog post for information about Go's ldflags parameter
+# https://www.digitalocean.com/community/tutorials/using-ldflags-to-set-version-information-for-go-applications
+MARCLI_LDFLAGS="-X 'main.Version=$MARCLI_VERSION'"
+
+GOOS=darwin GOARCH=amd64 go build -ldflags="$MARCLI_LDFLAGS" -o marcli
+GOOS=linux GOARCH=amd64 go build -ldflags="$MARCLI_LDFLAGS" -o marcli_linux
+GOOS=windows GOARCH=386 go build -ldflags="$MARCLI_LDFLAGS" -o marcli.exe
